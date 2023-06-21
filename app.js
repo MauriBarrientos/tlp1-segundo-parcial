8da78360ed0f8ee69dab67ea46579def6bb9552a
MauriBarrientos <mauribarrientos23@gmail.com>
2023-06-21 14:41:14 -0300

    Configuracion de archivo app.js

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 // Imports
 const cors = require('cors');
 const express = require('express');
-
 const path = require('path');
+require('dotenv').config();
+const {sequelize} = require('./db');
 
 const app = express();
 
 // Middlewares
-// TODO: Implementar middlewares
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use('/api', require('./routes/reserva.routes'));
+app.use(require('./routes/reserva.routes'));
 
-// TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
+
+app.use((req, res, next) => {
+    res.write(`<div>
+        <h1>404 - Ruta no encontrada</h1>
+        <hr>
+        <p>La pagina que intentas buscar no existe</p>
+        <p>Redireccionando a la página de inicio...</p>
+        <script>
+        (
+          () => setTimeout(() => {
+            window.location.href='http://localhost:${port}/tareas';
+           }, 3000)           
+        )();
+        </script>
+    </h1>`)
+});
+
 
 // Starting the server
 app.listen(45635, () => console.log('Server on port xxxx'));
